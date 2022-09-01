87711a0493a98a33d1366266a41a81ba694f4569
892294101 <892294101@qq.com>
2022-09-01 16:14:10 +0800

    init

---
+++ client.go
@@ -0,0 +1,76 @@
+package ddsrpc
+
+import (
+	"context"
+	"fmt"
+	"github.com/892294101/ddsrpc/proto"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"net"
+	"os"
+	"time"
+)
+
+type RpcClient struct {
+	rc   pcb.GreeterClient
+	conn *grpc.ClientConn
+}
+
+func (r *RpcClient) newRpcClient(port string) error {
+	// 连接grpc服务器
+	dopts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	ctx1, canal := context.WithTimeout(context.Background(), time.Second*5)
+	defer canal()
+	conn, err := grpc.DialContext(ctx1, net.JoinHostPort("localhost", port), dopts...)
+	if err != nil {
+		return errors.Errorf("did not connect: %v", err)
+	}
+
+	// 初始化Greeter服务客户端
+	c := pcb.NewGreeterClient(conn)
+	r.rc = c
+	r.conn = conn
+	return nil
+
+}
+
+func (r *RpcClient) Stop() (msg string, err error) {
+	// 初始化上下文，设置请求超时时间为15秒
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*6)
+	defer cancel()
+
+	// 调用Stop接口，发送一条消息
+	rcm, err := r.rc.Stop(ctx, &pcb.StopCommand{Instruction: 1})
+	if err != nil {
+		return "", err
+	}
+	fmt.Fprint(os.Stdout, string(rcm.ProcessName))
+	r.conn.Close()
+	return "", nil
+}
+
+func (r *RpcClient) Info() error {
+	// 初始化上下文，设置请求超时时间为15秒
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*6)
+	defer cancel()
+
+	// 调用info接口，发送一条消息
+	iac, err := r.rc.Info(ctx, &pcb.InfoAllCommand{Instruction: 1})
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprint(os.Stdout, string(iac.Content))
+	r.conn.Close()
+
+	return nil
+}
+
+func NewRpcClient(port string) (*RpcClient, error) {
+	c := new(RpcClient)
+	if err := c.newRpcClient(port); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/892294101/ddsrpc
+
+go 1.15
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	google.golang.org/grpc v1.49.0 // indirect
+)
